3c2627c004eb9d2db9353334d259c66c15a67276
jharte5 <jonathan.harte@codeimmersives.com>
2019-12-10 15:46:22 -0500

    car extends vehicle

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 class Vehicle {
+  constructor(name, wheelCount, maxSpeed) {
+    this.name = name;
+    this.wheelCount = wheelCount;
+    this.maxSpeed = maxSpeed;
+    this.speed = 0;
+  }
+  accelerate () {
+    let gas = this.maxSpeed / 3
+    if(this.speed === this.maxSpeed) {
+      return
+    }
+    this.speed += gas
+    
+  }
 }
 
-class Car {
+class Car extends Vehicle {
+  constructor(name) {
+    super()
+  }
+
 }
 
 class Ferrari {
 }
 
-
+const vehicle = new Vehicle('ford', 4, 120)
+vehicle.accelerate()
+vehicle.accelerate()
+vehicle
 module.exports = {
   Vehicle,
   Car,
